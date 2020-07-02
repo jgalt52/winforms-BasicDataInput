dbef92af3e0d04b7b6a5163b8dbe55ed8cdc4061
jgalt52 <42588645+jgalt52@users.noreply.github.com>
2020-07-02 11:44:54 -0600

    Add files via upload

---
+++ DataEntryProject/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataEntryProject
+{
+    public partial class frmDataEntry : Form
+    {
+
+        TimeSpan elapsedTime;
+        DateTime lastElapsed;
+        public frmDataEntry()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ClearFields()
+        {
+            txtName.Clear();
+            txtAddress.Clear();
+            txtCity.Clear();
+            txtState.Clear();
+            txtZip.Clear();
+            txtName.Focus();
+
+        }
+
+        private void timTimer_Tick(object sender, EventArgs e)
+        {
+            elapsedTime += DateTime.Now - lastElapsed;
+            lastElapsed = DateTime.Now;
+            txtTimer.Text = Convert.ToString(new TimeSpan(elapsedTime.Hours, elapsedTime.Minutes, elapsedTime.Seconds));
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnPause.Enabled = true;
+            timTimer.Enabled = true;
+            grbDataEntry.Enabled = true;
+            txtName.Focus();
+            lastElapsed = DateTime.Now;
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnPause.Enabled = false;
+            grbDataEntry.Enabled = false;
+            timTimer.Enabled = false;
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            string dataEntry;
+
+            if(txtName.Text.Equals("") || txtAddress.Text.Equals("") || txtCity.Text.Equals("") || txtState.Text.Equals("") || txtZip.Text.Equals(""))
+            {
+                MessageBox.Show("Each box must have an input", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtName.Focus();
+                return;
+            }
+            dataEntry = txtName.Text + "\r\n" + txtAddress.Text + "\r\n" + txtCity.Text + "\r\n" + txtState.Text + "\r\n" + txtZip.Text;
+
+            MessageBox.Show(dataEntry, "DataEntry",MessageBoxButtons.OK);
+            btnClear.PerformClick();
+        }
+
+        private void txtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string textBoxSender = ((TextBox)sender).Name;
+            if( e.KeyChar == 13)
+            {
+                switch(textBoxSender)
+                {
+                    case "txtName":
+                        txtAddress.Focus();
+                        break;
+                    case "txtAddress":
+                        txtCity.Focus();
+                        break;
+                    case "txtCity":
+                        txtState.Focus();
+                        break;
+                    case "txtState":
+                        txtZip.Focus();
+                        break;      
+                    case "txtZip":
+                        btnAccept.Focus();
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+
+            if(textBoxSender.Equals("txtZip"))
+            {
+                if((e.KeyChar >= '0' && e.KeyChar <='9') || e.KeyChar == 8)
+                {
+                    e.Handled = false;
+                }
+                else
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void btnButtons_Hover(object sender, EventArgs e)
+        {
+            ((Button)sender).BackColor = Color.Aqua;
+        }
+
+        private void btnButton_Leave(object sender, EventArgs e)
+        {
+            ((Button)sender).BackColor = default(Color);
+            ((Button)sender).UseVisualStyleBackColor = true;
+        }
+
+        private void btnStart_Hover(object sender, EventArgs e)
+        {
+            btnStart.BackColor = Color.Green;
+        }
+
+        private void btnStart_Leave(object sender, EventArgs e)
+        {
+            btnStart.BackColor = default(Color);
+            btnStart.UseVisualStyleBackColor = true;
+        }
+    }
+}
